0b4327e23edb76ee1a78766c7b658dc24e231158
Guy Orlov <126164694+GuyOrlov@users.noreply.github.com>
2024-12-01 21:19:23 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
 */
-//
+// 
 // Scripts
 // 
 
@@ -30,5 +30,32 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
+
+    // Theme Switcher functionality
+    const themeSwitcher = document.querySelector('.theme-switcher input[type="checkbox"]');
+    
+    // Apply initial theme based on current body class
+    if (document.body.classList.contains('bg-dark-theme')) {
+        themeSwitcher.checked = true;
+    }
+
+    // Event listener for theme switcher toggle
+    themeSwitcher.addEventListener('change', function() {
+        toggleTheme();
+    });
+
+    // Function to toggle between light and dark theme
+    function toggleTheme() {
+        var body = document.body;
+        
+        // Toggle between light and dark themes
+        if (body.classList.contains('bg-light-theme')) {
+            body.classList.remove('bg-light-theme');
+            body.classList.add('bg-dark-theme');
+        } else {
+            body.classList.remove('bg-dark-theme');
+            body.classList.add('bg-light-theme');
+        }
+    }
 
 });

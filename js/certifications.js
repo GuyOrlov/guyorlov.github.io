6985dfbfa8edad78dfb748f16725f18861765ad3
Guy Orlov <126164694+GuyOrlov@users.noreply.github.com>
2024-12-06 19:53:04 +0000

    Update certifications.js

--- js/certifications.js
+++ js/certifications.js
@@ -1,34 +1,12 @@
-// Define the certifications content
-const certificationsHTML = `
+const certificationsData = `
     <ul class="fa-ul mb-0">
-        <li>
-            <span class="fa-li"><i class="fa-solid fa-medal"></i></span>
-            PowerBI PL-300 - 2024
-        </li>
-        <li>
-            <span class="fa-li"><i class="fa-solid fa-medal"></i></span>
-            Career Essentials in GitHub Professional Certificate - 2024
-        </li>
-        <li>
-            <span class="fa-li"><i class="fa-solid fa-medal"></i></span>
-            BPP NVQ Level 3 Data Technician - 2023
-        </li>
-        <li>
-            <span class="fa-li"><i class="fa-solid fa-medal"></i></span>
-            Coursera Google Data Analytics Professional Certificate - 2023
-        </li>
-        <li>
-            <span class="fa-li"><i class="fa-solid fa-medal"></i></span>
-            Paragon Skills NVQ Level 3 Business Administration - 2020
-        </li>
-        <li>
-            <span class="fa-li"><i class="fa-solid fa-medal"></i></span>
-            Sheffield Hallam University, BSc, Computing Visualisation - 2009
-        </li>
+        <li><span class="fa-li"><i class="fa-solid fa-medal"></i></span>PowerBI PL-300 - 2024</li>
+        <li><span class="fa-li"><i class="fa-solid fa-medal"></i></span>Career Essentials in GitHub Professional Certificate - 2024</li>
+        <li><span class="fa-li"><i class="fa-solid fa-medal"></i></span>BPP NVQ Level 3 Data Technician - 2023</li>
+        <li><span class="fa-li"><i class="fa-solid fa-medal"></i></span>Coursera Google Data Analytics Professional Certificate - 2023</li>
+        <li><span class="fa-li"><i class="fa-solid fa-medal"></i></span>Paragon Skills NVQ Level 3 Business Administration - 2020</li>
+        <li><span class="fa-li"><i class="fa-solid fa-medal"></i></span>Sheffield Hallam University, BSc, Computing Visualisation - 2009</li>
     </ul>
 `;
 
-// Insert the certifications content into the container
-if (certificationsContainer) {
-    certificationsContainer.innerHTML = certificationsHTML;
-}
+document.getElementById('certifications-container').innerHTML = certificationsData;

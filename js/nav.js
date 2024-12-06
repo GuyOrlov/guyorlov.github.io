11bebbefee7456ee20906f27bfac69ce24f76de8
Guy Orlov <126164694+GuyOrlov@users.noreply.github.com>
2024-12-06 20:05:41 +0000

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,6 @@
-fetch('/templates/nav.html')
+fetch('/nav.html')
     .then(response => response.text())
     .then(data => {
+        console.log(data); // Debugging line
         document.getElementById('navbar-placeholder').innerHTML = data;
     });

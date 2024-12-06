ef36376044647d0dbda2db8159e6d0498e88e584
Guy Orlov <126164694+GuyOrlov@users.noreply.github.com>
2024-12-06 19:47:04 +0000

    Update certifications.js

--- js/certifications.js
+++ js/certifications.js
@@ -1,15 +1,37 @@
-const certifications = [
-    { title: "PL-300: Microsoft Power BI Data Analyst", institution: "Microsoft", date: "2024" },
-    { title: "SQL Basics Certification", institution: "Codecademy", date: "2023" },
-];
-
+// Select the certifications container
 const certificationsContainer = document.getElementById('certifications-container');
-certifications.forEach(cert => {
-    const certDiv = document.createElement('div');
-    certDiv.classList.add('certification');
-    certDiv.innerHTML = `
-        <h3>${cert.title}</h3>
-        <p>${cert.institution} - ${cert.date}</p>
-    `;
-    certificationsContainer.appendChild(certDiv);
-});
+
+// Define the certifications content
+const certificationsHTML = `
+    <ul class="fa-ul mb-0">
+        <li>
+            <span class="fa-li"><i class="fa-solid fa-medal"></i></span>
+            PowerBI PL-300 - 2024
+        </li>
+        <li>
+            <span class="fa-li"><i class="fa-solid fa-medal"></i></span>
+            Career Essentials in GitHub Professional Certificate - 2024
+        </li>
+        <li>
+            <span class="fa-li"><i class="fa-solid fa-medal"></i></span>
+            BPP NVQ Level 3 Data Technician - 2023
+        </li>
+        <li>
+            <span class="fa-li"><i class="fa-solid fa-medal"></i></span>
+            Coursera Google Data Analytics Professional Certificate - 2023
+        </li>
+        <li>
+            <span class="fa-li"><i class="fa-solid fa-medal"></i></span>
+            Paragon Skills NVQ Level 3 Business Administration - 2020
+        </li>
+        <li>
+            <span class="fa-li"><i class="fa-solid fa-medal"></i></span>
+            Sheffield Hallam University, BSc, Computing Visualisation - 2009
+        </li>
+    </ul>
+`;
+
+// Insert the certifications content into the container
+if (certificationsContainer) {
+    certificationsContainer.innerHTML = certificationsHTML;
+}

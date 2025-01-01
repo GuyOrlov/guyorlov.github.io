72eaba87a9350d40f5dc876ae82d4676a96973b6
Guy Orlov <126164694+GuyOrlov@users.noreply.github.com>
2025-01-01 13:21:23 +0000

    Update seo.js

--- js/seo.js
+++ js/seo.js
@@ -1,14 +1,16 @@
 document.head.insertAdjacentHTML(
     'beforeend',
     `
-    <meta name="description" content="Empowering businesses with actionable insights and accessible data visualisation. Guy Orlov, Data Analyst and Accessibility Expert in the West Midlands."/>
+    <meta name="description" content="Specialised data analytics and accessibility services by Guy Orlov, based in the West Midlands. Expert in dashboards, data visualisation, and actionable business insights.">
+    <meta name="keywords" content="Data Analyst, Accessible Dashboards, West Midlands, Data Cleaning, Business Insights, Power BI Expert, Inclusive Visualisation, Data Accessibility">
     <meta name="author" content="Guy Orlov" />
-    <meta name="keywords" content="Data Analyst, Accessibility Expert, Data Visualisation, Dashboards, West Midlands, Guy Orlov, Business Insights, Data Analytics Services, Power BI, Excel, Inclusive Data" />
+    
     <meta property="og:title" content="Guy Orlov | Data Analyst & Accessibility Expert | West Midlands" />
     <meta property="og:description" content="Unlock the power of accessible data with Guy Orlov. Transform data into actionable insights with inclusive and visually engaging dashboards." />
     <meta property="og:image" content="img/profile.jpg" />
     <meta property="og:url" content="https://guyorlov.com" />
     <meta property="og:type" content="website" />
+    
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content="Guy Orlov | Data Analyst | Accessibility & Dashboards" />
     <meta name="twitter:description" content="Empowering businesses with accessible and impactful data insights in the West Midlands." />
